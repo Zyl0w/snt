4c2ec628231618da616a994e1d8eedd9ba62705a
Zylow <90773297+Zyl0w@users.noreply.github.com>
2023-09-23 15:20:05 +0200

    Uploaded the site

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typing", {
+    strings: [`<u>Developpers<p class="inline">_</p></u>`],
+    typeSpeed: 50,
+    backSpeed: 0,
+    cursorChar: '|',
+    backDelay: 500,
+    startDelay: 10,
+    smartBackspace: true,
+    loop: false
+})
